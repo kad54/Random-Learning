6b0bd3c47d82f1b876db6f4edc59300b28da97e0
Katarzyna Dylewska <kad54@cam.ac.uk>
2023-07-11 20:23:56 +0100

    Update search_replace_gui.py

--- search_replace_gui.py
+++ search_replace_gui.py
@@ -1,63 +1,91 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 
 def search_and_replace():
-    search_phrase = search_entry.get()
-    replace_phrase = replace_entry.get()
-    
-    file_path = file_entry.get()
-    
+    search_phrase = search_text.get("1.0", tk.END).strip()
+    replace_phrase = replace_text.get("1.0", tk.END).strip()
+
+    file_path = file_entry.get().strip()
+
+    if not search_phrase or not replace_phrase or not file_path:
+        result_label.config(text='Please provide all required information.')
+        return
+
     try:
         with open(file_path, 'r') as file:
             file_content = file.read()
-        
+
         replaced_content = file_content.replace(search_phrase, replace_phrase)
-        
+
         with open(file_path, 'w') as file:
             file.write(replaced_content)
-        
+
         result_label.config(text='Replacement completed.')
-        
+
     except FileNotFoundError:
         result_label.config(text='File not found.')
-    
+
 def open_file_dialog():
     file_path = filedialog.askopenfilename(filetypes=[('Text Files', '*.txt')])
-    file_entry.delete(0, tk.END)
-    file_entry.insert(0, file_path)
+    if file_path:
+        file_entry.delete(0, tk.END)
+        file_entry.insert(0, file_path)
 
 # Create the main window
 window = tk.Tk()
 window.title('Search and Replace')
-window.geometry('400x200')
+window.geometry('400x400')
 
-# Create the search phrase label and entry
-search_label = tk.Label(window, text='Search phrase:')
-search_label.pack()
-search_entry = tk.Entry(window)
-search_entry.pack()
+# Create a style for ttk widgets
+style = ttk.Style()
+style.theme_use('default')
+style.configure('TLabel', font=('Arial', 11))
+style.configure('TButton', font=('Arial', 11))
 
-# Create the replace phrase label and entry
-replace_label = tk.Label(window, text='Replace phrase:')
-replace_label.pack()
-replace_entry = tk.Entry(window)
-replace_entry.pack()
+# Create a frame for search phrase
+search_frame = ttk.Frame(window)
+search_frame.pack(pady=20)
 
-# Create the file selection button
-file_button = tk.Button(window, text='Select File', command=open_file_dialog)
-file_button.pack()
+search_label = ttk.Label(search_frame, text='Search phrase:')
+search_label.pack(side=tk.LEFT, padx=5)
 
-# Create the file entry field
-file_entry = tk.Entry(window)
-file_entry.pack()
+search_text = tk.Text(search_frame, height=5, width=30, font=('Arial', 11))
+search_text.pack(side=tk.LEFT, padx=5)
+
+# Create a frame for replace phrase
+replace_frame = ttk.Frame(window)
+replace_frame.pack(pady=10)
+
+replace_label = ttk.Label(replace_frame, text='Replace phrase:')
+replace_label.pack(side=tk.LEFT, padx=5)
+
+replace_text = tk.Text(replace_frame, height=5, width=30, font=('Arial', 11))
+replace_text.pack(side=tk.LEFT, padx=5)
+
+# Create the file selection section
+file_frame = ttk.Frame(window)
+file_frame.pack(pady=10)
+
+file_label = ttk.Label(file_frame, text='File:')
+file_label.pack(side=tk.LEFT, padx=5)
+
+file_entry = ttk.Entry(file_frame, width=30, font=('Arial', 11))
+file_entry.pack(side=tk.LEFT, padx=5)
+
+file_button = ttk.Button(file_frame, text='Select', command=open_file_dialog)
+file_button.pack(side=tk.LEFT, padx=5)
 
 # Create the replace button
-replace_button = tk.Button(window, text='Replace', command=search_and_replace)
-replace_button.pack()
+replace_button = ttk.Button(window, text='Replace', command=search_and_replace)
+replace_button.pack(pady=20)
 
 # Create the result label
-result_label = tk.Label(window, text='')
+result_label = ttk.Label(window, text='', font=('Arial', 11), foreground='green')
 result_label.pack()
+
+# Configure window padding
+window.configure(padx=20, pady=20)
 
 # Start the main event loop
 window.mainloop()
